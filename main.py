4d561bd501a7a518ce0aff186f4936570b6052df
VinayGupta <91736791+WinayGupta777@users.noreply.github.com>
2021-10-29 22:37:13 +0530

    final commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# C++ Code !!
+#####################
+# i = 1
+# do
+# {
+#     cout<<"Hello LinuxWorld";
+#     i++;
+# }
+# while( i > 10 );  //False
+
+
+# Python Code !!
+####################
+i = 1
+while(True):
+    print("Hello LinuxWorld")
+    i = i + 1
+    if(i < 10):
+        break
